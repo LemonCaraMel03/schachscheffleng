2aeece0b6ba568a908b408709da59b0827bb9e88
mt454 <mt454@student.le.ac.uk>
2023-05-27 18:43:38 +0100

    Mostly finished all the layouts.

---
+++ js/news.js
@@ -0,0 +1,21 @@
+var newsTitle= "Last Saturday Open!";
+
+const news = [
+    "20/05/2023 - 01/07/2023",
+    "20/05/2023 - 01/07/2023",
+    "20/05/2023 - 01/07/2023"
+];
+
+function update_news_bar() {
+    if (newsTitle != "" && news.length != 0) {
+        document.getElementById("news-title").innerHTML = newsTitle;
+
+        var newsContent = "";
+        for (let e of news) {
+            newsContent += "<li>" + e + "</li>";
+        }
+        document.getElementById("news").innerHTML = newsContent;
+
+        document.getElementById("top-bar").style.marginTop= "44px";
+    };
+}
